d5c9f8afb8a430d00008095d2d905b1fb27c48c2
Aaron <devilfruits1999@gmail.com>
2019-10-21 16:21:35 -0400

    Added Profile icon and top5 mastery icons

--- LeagueWorkBook/MainWindow.xaml.cs
+++ LeagueWorkBook/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using MingweiSamuel.Camille.Enums;
 using MingweiSamuel.Camille.SummonerV4;
 using MingweiSamuel.Camille.Util;
 using MingweiSamuel.Camille.SpectatorV4;
+using MingweiSamuel.Camille.ChampionMasteryV4;
+using System.Text.RegularExpressions;
 
 namespace LeagueWorkBook
 {
@@ -25,26 +27,65 @@ namespace LeagueWorkBook
     /// </summary>
     public partial class MainWindow : Window
     {
-        RiotApi riotApi = RiotApi.NewInstance("RGAPI-e3c14311-1f44-437b-993b-ac6286c737c2");
+        RiotApi riotApi = RiotApi.NewInstance("RGAPI-c3510fc1-fd31-4c00-9df1-8579b88c0b76");
         Summoner searchedSummoner;
         CurrentGameInfo CurrentGameInfo;
-
+        ChampionMastery[] championMastery;
+        List<Image> champMasteryImages = new List<Image>();
         public MainWindow()
         {
             InitializeComponent();
 
-            
-        }
+            //adds image objects to an array
+            champMasteryImages.Add(champMasteryIcon1);
+            champMasteryImages.Add(champMasteryIcon2);
+            champMasteryImages.Add(champMasteryIcon3);
+            champMasteryImages.Add(champMasteryIcon4);
+            champMasteryImages.Add(champMasteryIcon5);
 
+        }
+        void ChangeProfileIcon(int iconID) 
+        {
+            ProfileIconImage.Source = new BitmapImage(new Uri(@"/Images/profileicon/" + iconID + ".png", UriKind.Relative));
+        }
         private void SummonerNameSearchBtn_Click(object sender, RoutedEventArgs e)
         {
-            searchedSummoner = riotApi.SummonerV4.GetBySummonerName(Region.NA, SummonerNameSearchField.Text);
-            CurrentGameInfo = riotApi.SpectatorV4.GetCurrentGameInfoBySummoner(Region.NA,searchedSummoner.Id);
-            DisplayMatch();
+            if (SummonerNameSearchField.Text != "")
+            {
+                try
+                {
+                    searchedSummoner = riotApi.SummonerV4.GetBySummonerName(Region.NA, SummonerNameSearchField.Text);
+                    //CurrentGameInfo = riotApi.SpectatorV4.GetCurrentGameInfoBySummoner(Region.NA,searchedSummoner.Id);
+                    championMastery = riotApi.ChampionMasteryV4.GetAllChampionMasteries(Region.NA, searchedSummoner.Id);
+                }
+                catch (NullReferenceException ex)
+                {
+                    MatchupNotes.Text = "Summoner Not Found!";
+                }
+                if (searchedSummoner != null)
+                {
+                    DisplayMatch();
+                }
+                else
+                {
+                    ChangeProfileIcon(0);
+                }
+            }
+            
         }
         void DisplayMatch() 
         {
-            
+            //clears the notes
+            MatchupNotes.Text = "";
+            for (int i = 0; i < 5; i++)
+            {
+                ChampionMastery mastery = championMastery[i];
+                Champion champion = (Champion)mastery.ChampionId;
+                MatchupNotes.Text = string.Concat(MatchupNotes.Text, (i+1)+")",champion.Name(), $": {mastery.ChampionPoints} \n");
+
+                champMasteryImages[i].Source = new BitmapImage(new Uri(@"/Images/champion/" + champion.Name().Replace(" ", string.Empty) + ".png", UriKind.Relative));
+            }
+            ChangeProfileIcon(searchedSummoner.ProfileIconId);
         }
     }
 }
